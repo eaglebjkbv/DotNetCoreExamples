89cb5cf812acde1d1b5a266d3815172a4c9ad6fd
eaglebjkbv <bbvardal@gmail.com>
2019-01-01 23:34:08 +0300

    Class Example added

---
+++ ClassExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassExample
+{
+    class Ogrenci{
+        public int id;
+        public string adi;
+        public string soyadi;
+        public DateTime dogumTarihi;
+
+        public string yaz(){
+            
+            return string.Format("Numarası: {0} Adi : {1} Soyadı : {2} Doğum Tarihi: {3} ",id,adi,soyadi,dogumTarihi);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci=new Ogrenci();
+            ogrenci.id=1;
+            ogrenci.adi="Mehmet";
+            ogrenci.soyadi="Öztürk";
+            ogrenci.dogumTarihi=new DateTime(2019,1,1);
+
+            Console.WriteLine(ogrenci.yaz());
+        }
+    }
+}
