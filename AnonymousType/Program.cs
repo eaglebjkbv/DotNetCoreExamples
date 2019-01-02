4f6ed37f8216d5dcec8f82ff0879093de37f0332
unknown <bbvardal@gmail.com>
2019-01-02 11:09:52 +0300

    Anonymous Type example added

---
+++ AnonymousType/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AnonymousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ogrenci=new {num=1,ad="Hasan",soyad="Can" }; // Burada oluşacak verinin class tasımalaması yapılmamış "var ogrenci" bu anonymous type dır. 
+            System.Console.WriteLine($"Öğrenci No: {ogrenci.num} \nÖğrenci Adi: {ogrenci.ad}");
+
+            // Örnek bir Anonymous dizi oluşturma
+
+            var hayvanlar=new[] {
+                                new {adi="Köpek",kilosu=10},
+                                new {adi="Kedi", kilosu=5}
+
+                            };
+        if(hayvanlar!=null){
+                foreach (var item in hayvanlar)
+                {
+                    System.Console.WriteLine($"Hayvan Adı : {item.adi} , Hayvanın Kilosu : {item.kilosu}");
+                }
+        }
+        }
+    }
+}
