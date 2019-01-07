655b0eba7e6bbf49ee7ca77c5e48e74a33cc3bb1
Bulent Vardal <bbvardal@gmail.com>
2019-01-07 17:10:02 +0300

    Interface Example Added

---
+++ InterfaceDotnet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterfaceDotnet
+{
+
+    interface IHayvan{
+        string sesver();
+    }
+
+    class Kopek:IHayvan{
+        public string sesver()
+        {
+            return "hav Hav";
+        }
+    }
+    class Kedi:IHayvan{
+        public string sesver(){
+            return "Miyav";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kopek dobi=new Kopek();
+            
+            System.Console.WriteLine($"Kopekler {dobi.sesver()} derler");   
+
+            Kedi mirnav=new Kedi();
+            System.Console.WriteLine($"Kediler {mirnav.sesver()} derler");
+        }
+    }
+}
