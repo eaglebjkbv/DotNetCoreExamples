6e5e1ae1cea37d50826c07f663092f1c488db60a
Bulent Vardal <bbvardal@gmail.com>
2019-01-08 17:26:01 +0300

    Small changes on NamespaceClass

--- NameSpaceClass/Program.cs
+++ NameSpaceClass/Program.cs
@@ -9,6 +9,10 @@ namespace NameSpaceClass
         {
             Kopek dobi=new Kopek();
             System.Console.WriteLine($"Köpekler {dobi.sesver()} derler...");
+            Kedi tekir=new Kedi();
+            System.Console.WriteLine($"Kediler {tekir.sesver()} derler...");
+
+
         }
     }
 }
